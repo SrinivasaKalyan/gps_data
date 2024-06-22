41d27236aef339aee91cd452b1dedc2562f9b960
Kalyan Kanchumarthi <109733865+SrinivasaKalyan@users.noreply.github.com>
2024-06-22 10:20:48 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -16,12 +16,9 @@ def main():
     st.set_page_config(layout="wide")
     EDA_tasks = ["1.distinguish attributes","2.Data Cleaning", "3.Speed","4.Maps","5.Entry & Exit points"]
     choice = st.sidebar.radio("select tasks:", EDA_tasks)
-    file_format = st.radio('Select file format:', ('csv', 'excel'), key='file_format')
 
 
-
-    
-    df = pd.read_excel("report(5).xlsx")
+    df = pd.read_excel("report (5).xlsx")
     st.dataframe(df.head())
     # df.drop(columns={'Unnamed: 6'}, inplace=True)
     df.rename(columns={'Unnamed: 0': 'Valid', 'Unnamed: 1': 'Time', 'Unnamed: 2': 'Lat', 'Unnamed: 3': 'Long',
